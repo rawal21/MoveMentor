194dff19e20117469717edac8204d256825b5e28
dikshitkumar <dikshitrawal923@gmail.com>
2024-11-02 12:08:33 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const MONGO_URL = process.env.MONGO_URL;
 const userRoutes = require("./routes/user.js")
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://glittering-pithivier-f709d6.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
