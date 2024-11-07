32dc6481e4672f0bb6f1b470905151d97390d97a
dikshitkumar <dikshitrawal923@gmail.com>
2024-11-07 20:22:24 +0530

    change in backend url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://movementor-xsgl.onrender.com/api/",
+  baseURL: "https://movementor.netlify.app/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
