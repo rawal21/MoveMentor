76ba43a4a28cf827622496ade872a76c966517de
dikshitkumar <dikshitrawal923@gmail.com>
2024-11-08 22:51:59 +0530

    change in frotend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://movementor-unwc.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
